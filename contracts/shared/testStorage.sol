70c7e2086db6b97be693c3e334b6a5f0cef5df79
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2022-10-11 16:40:50 -0400

    apply pr changes requested and bump version

--- contracts/shared/testStorage.sol
+++ contracts/shared/testStorage.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.0;
 
 contract TestStorage {
     // Storage use for the tests.
-    bool _IS_SANDBOX;
-    uint8 _LOG_LEVEL;
+    bool internal _IS_SANDBOX;
+    uint8 internal _LOG_LEVEL;
 }
--- contracts/utils/script.sol
+++ contracts/utils/script.sol
@@ -2,7 +2,6 @@
 pragma solidity >=0.6.0 <0.9.0;
 
 import "./console.sol";
-import "./console2.sol";
 import "./stdJson.sol";
 
 abstract contract Script {
