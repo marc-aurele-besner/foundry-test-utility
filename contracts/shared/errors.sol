6fa1e7d7df283402525aa38c9d727fcbbac14b9c
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2022-10-13 09:56:32 -0400

    change how errors work to allow more easily inheritance and override

--- contracts/shared/errors.sol
+++ contracts/shared/errors.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
+pragma solidity >=0.8.0 <0.9.0;
 
 import { Vm } from '../utils/vm.sol';
 import { DSTest } from '../utils/test.sol';
@@ -7,26 +7,33 @@ import { DSTest } from '../utils/test.sol';
 contract Errors is DSTest {
     Vm public constant vm = Vm(address(uint160(uint256(keccak256('hevm cheat code')))));
 
-    mapping(uint16 => string) internal _errors;
+    string constant public REVERT_STATUS_SUCCESS = '[success]';
+    string constant public REVERT_STATUS_SKI_VALIDATION = '[skip]';
 
-    // Add a revert error to the enum of errors.
-    enum BasicRevertStatus {
-        Success,
-        SkipValidation
-    }
+    string[] internal _errors;
 
-    // Associate your error with a revert message and add it to the mapping.
+    // Associate your error with a mapping
     constructor() {
-        // _errors[RevertStatus.ErrorExample] = 'Error: Error Message';
+        _errors.push(REVERT_STATUS_SUCCESS);
+        _errors.push(REVERT_STATUS_SKI_VALIDATION);
     }
 
-    // Return the error message associated with the error.
-    function _verify_revertCall(uint16 revertType_) internal view virtual returns (string storage) {
-        return _errors[revertType_];
+    // Expect a revert error if the revert type is not success or skip.
+    function verify_revertCall(string calldata revertType_) public virtual {
+        for (uint256 i = 0; i < _errors.length; i++) {
+            if (_validateString(revertType_, _errors[i])) {
+                if (!_validateString(revertType_, REVERT_STATUS_SUCCESS) && !_validateString(revertType_, REVERT_STATUS_SKI_VALIDATION))
+                    vm.expectRevert(bytes(revertType_));
+            }
+        }
     }
-
-    // Expect a revert error if the revert type is not success.
-    function verify_revertCall(uint16 revertType_) public virtual {
-        if (revertType_ > 1) vm.expectRevert(bytes(_verify_revertCall(revertType_)));
+    
+    /**
+    * @dev Validate string match a other string (A==B)
+    * @param strA - String A
+    * @param strB - String B
+    */
+    function _validateString(string calldata strA, string memory strB) internal pure virtual returns (bool) {
+        return keccak256(abi.encodePacked(strA)) == keccak256(abi.encodePacked(strB));
     }
 }
