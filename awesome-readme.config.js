579f2b995aad233de226d5d7969b8f6fbbb974cc
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2022-10-22 17:56:41 -0400

    improve prettierignore settings and add awesome-readme config

---
+++ awesome-readme.config.js
@@ -0,0 +1,54 @@
+module.exports = {
+    figlet: ``,
+    root_license: `[![npm version](https://badge.fury.io/js/foundry-test-utility.svg)](https://badge.fury.io/js/foundry-test-utility)`,
+    root_header: `
+
+This package aim to deliver a set of reusable contracts and helper function that can be reused and override in your own [Foundry](https://github.com/foundry-rs) tests.
+
+## To add yo your project
+
+First install the package using NPM, Yarn or Forge Install.
+
+\`\`\`
+npm i foundry-test-utility
+\`\`\`
+
+or
+
+\`\`\`
+yarn add foundry-test-utility
+\`\`\`
+
+or
+
+\`\`\`
+foundry install marc-aurele-besner/foundry-test-utility
+\`\`\`
+
+Then add this package path in remapping.text
+
+For NPM/Yarn
+
+\`\`\`
+foundry-test-utility/contracts/=node_modules/foundry-test-utility/contracts
+\`\`\`
+
+For Forge Install
+
+\`\`\`
+foundry-test-utility/contracts/=libs/foundry-test-utility/contracts
+
+\`\`\`
+`,
+    root_body: `
+    
+## Reference
+
+This repository include helper contracts from:
+
+-   [Forge-std](https://github.com/foundry-rs/forge-std)
+-   [DS-Test](https://github.com/dapphub/ds-test)
+  
+`,
+    root_footer: `## Don't hesitate to contribute to this project.`
+};
